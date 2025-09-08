a221e073334f65efa88bc0abb65aec1168cbcbe7
Siddhartha (Sid) Somani <sidsomani2005@gmail.com>
2025-09-07 20:18:02 -0700

    loops and conditionals

--- main.go
+++ main.go
@@ -4,4 +4,16 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello World!")
+
+	if x := 10; x > 5 {
+		fmt.Println("x is gt 5")
+	} else {
+		fmt.Println("x is not gt 5")
+	}
+
+	i := 0
+	for i < 10 {
+		fmt.Println(i)
+		i++
+	}
 }
